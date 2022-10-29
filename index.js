c71d6438a5152795d008fc43a62d5c35038ad2c8
LuisTorres <luistorresse97@gmail.com>
2022-10-29 18:47:56 -0500

    nuevo

---
+++ index.js
@@ -0,0 +1,32 @@
+import PORT from "./config.js";
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+app.use(express.static("build"));
+app.use(cors());
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.listen(PORT);
